22390b719323f9f3cdc86650994bb05e6ff0b3a0
android-jian <2424055636@qq.com>
2017-03-15 23:22:52 +0800

    集成ShareSDK 微信分享待实现

--- app/src/main/java/com/jian/android/screencut/ScreenCutActivity.java
+++ app/src/main/java/com/jian/android/screencut/ScreenCutActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,6 +17,9 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
+
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 public class ScreenCutActivity extends AppCompatActivity {
 
@@ -24,11 +30,17 @@ public class ScreenCutActivity extends AppCompatActivity {
     private ArrayList<IconBean> mDatas;
     private ImageView iv_icon_show;
     private EditText et_input;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_cut);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        ShareSDK.initSDK(this);
 
         imageView= (ImageView) findViewById(R.id.iv_image);
         iv_icon_show = (ImageView) findViewById(R.id.iv_icon_show);
@@ -91,4 +103,57 @@ public class ScreenCutActivity extends AppCompatActivity {
         mRecycler.setAdapter(new MyAdapter(mDatas,iv_icon_show,et_input));
     }
 
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
+        oks.setTitle("Share");
+        // titleUrl是标题的网络链接，QQ和QQ空间等使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+        // 启动分享GUI
+        oks.show(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.share:
+
+                showShare();
+
+                break;
+
+            default:
+                break;
+        }
+
+        return true;
+    }
 }

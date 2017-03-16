43b4b45a4ba6b67ac49ed9191ed15985a29de66d
android-jian <2424055636@qq.com>
2017-03-16 22:42:21 +0800

    完成截屏及分享功能

--- app/src/main/java/com/jian/android/screencut/ScreenCutActivity.java
+++ app/src/main/java/com/jian/android/screencut/ScreenCutActivity.java
@@ -2,7 +2,7 @@ package com.jian.android.screencut;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,6 +18,8 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import cn.sharesdk.framework.ShareSDK;
@@ -141,7 +143,12 @@ public class ScreenCutActivity extends AppCompatActivity implements View.OnClick
                         mDatas.get(i).isSelected=false;
                     }
                     myAdapter.notifyDataSetChanged();
+
                 }
+
+                mOne.setBackgroundColor(android.graphics.Color.parseColor("#CCCCCC"));
+                mTwo.setBackgroundColor(android.graphics.Color.parseColor("#F2F2F2"));
+                mThree.setBackgroundColor(android.graphics.Color.parseColor("#F2F2F2"));
 
                 break;
 
@@ -156,6 +163,10 @@ public class ScreenCutActivity extends AppCompatActivity implements View.OnClick
                     myAdapter.notifyDataSetChanged();
                 }
 
+                mOne.setBackgroundColor(android.graphics.Color.parseColor("#F2F2F2"));
+                mTwo.setBackgroundColor(android.graphics.Color.parseColor("#CCCCCC"));
+                mThree.setBackgroundColor(android.graphics.Color.parseColor("#F2F2F2"));
+
                 break;
 
             case R.id.iv_three:
@@ -169,6 +180,10 @@ public class ScreenCutActivity extends AppCompatActivity implements View.OnClick
                     myAdapter.notifyDataSetChanged();
                 }
 
+                mOne.setBackgroundColor(android.graphics.Color.parseColor("#F2F2F2"));
+                mTwo.setBackgroundColor(android.graphics.Color.parseColor("#F2F2F2"));
+                mThree.setBackgroundColor(android.graphics.Color.parseColor("#CCCCCC"));
+
                 break;
 
             default:
@@ -176,28 +191,14 @@ public class ScreenCutActivity extends AppCompatActivity implements View.OnClick
         }
     }
 
+
     private void showShare() {
-        ShareSDK.initSDK(this);
+
         OnekeyShare oks = new OnekeyShare();
         //关闭sso授权
         oks.disableSSOWhenAuthorize();
-
-        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
-        oks.setTitle("Share");
-        // titleUrl是标题的网络链接，QQ和QQ空间等使用
-        oks.setTitleUrl("http://sharesdk.cn");
-        // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
-        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
-        // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
-        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
-        // site是分享此内容的网站名称，仅在QQ空间使用
         oks.setSite(getString(R.string.app_name));
-        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
+        oks.setImagePath("/sdcard/icon.png");
 
         // 启动分享GUI
         oks.show(this);
@@ -218,7 +219,35 @@ public class ScreenCutActivity extends AppCompatActivity implements View.OnClick
 
             case R.id.share:
 
-                showShare();
+                View view=getWindow().getDecorView().findViewById(R.id.fl_show);
+                view.setDrawingCacheEnabled(true);
+                view.buildDrawingCache();
+                Bitmap bitmap = view.getDrawingCache();
+
+                try {
+                    String status = Environment.getExternalStorageState();
+                    // 判斷SD卡是否存在
+                    if (status.equals(Environment.MEDIA_MOUNTED)) {
+
+                        String path=Environment.getExternalStorageDirectory().toString();//获取根目录
+
+                        FileOutputStream out=null;
+                        try {
+                            out=new FileOutputStream(new File(path,"icon.png"));
+                            bitmap.compress(Bitmap.CompressFormat.PNG,100,out);
+
+                            out.flush();
+                            out.close();
+
+                            showShare();
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
                 break;
 
